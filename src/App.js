ded203f38d40375d216f46f61dbf2d8edefa6b56
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2021-08-20 18:27:36 +0200

    add another route for ProductDetail page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
 
 import MainHeader from './components/MainHeader';
 
@@ -15,6 +16,9 @@ function App() {
         </Route>
         <Route path="/products">
           <Products />
+        </Route>
+        <Route path="/product-detail">
+          <ProductDetail />
         </Route>
       </main>
     </div>
