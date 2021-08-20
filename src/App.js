f97f0a7247a33ba9088f9e0ae53babc9336b5337
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2021-08-20 19:01:16 +0200

    added redirect router component in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -12,6 +12,9 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
           <Route exact path="/welcome">
             <Welcome />
           </Route>
