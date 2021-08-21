57060e5da2d5e860a011a1089b0ab507791fd743
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2021-08-21 09:26:12 +0200

    added useHistory to NewQuote.js, return user to /quotes route

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,9 +1,16 @@
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandle = (quoteData) => {
     console.log(quoteData);
+
+    history.push('/quotes');
   };
+
   return <QuoteForm onAddQuote={addQuoteHandle} />;
 };
 
