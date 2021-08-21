0d892da6f4b4a7da45eee0837a5dae1b52b049ce
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2021-08-21 09:20:52 +0200

    created NotFound.js page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import AllQuotes from './pages/AllQuotes';
 import QuoteDetail from './pages/QuoteDetail';
+import NotFound from './pages/NotFound';
 import NewQuote from './pages/NewQuote';
 import Layout from './components/layout/Layout';
 
@@ -20,6 +21,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="center">
+      <p>Page not found!</p>
+    </div>
+  );
+};
+
+export default NotFound;
