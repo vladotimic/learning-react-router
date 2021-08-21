5b537b7b31a25d78f742cc6cdd52302b5770688b
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2021-08-21 09:17:35 +0200

    updated quotedetail.js page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { useParams, Route } from 'react-router-dom';
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
 
 const DUMMY_QUOTES = [
@@ -11,10 +12,15 @@ const DUMMY_QUOTES = [
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
