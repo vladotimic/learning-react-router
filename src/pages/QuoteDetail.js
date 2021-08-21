87681c9df6c9c4a40499cde39ced8e51ce5decda
Vladislav Timić <32008359+vladotimic@users.noreply.github.com>
2021-08-21 09:12:44 +0200

    added dummy_data to QuoteDetail.js

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -3,6 +3,11 @@ import { useParams, Route } from 'react-router-dom';
 
 import Comments from '../components/comments/Comments';
 
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
 const QuoteDetail = () => {
   const params = useParams();
 
